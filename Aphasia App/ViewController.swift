e67ebc5c5a016ba8317d2acf51c59c57ce93d8f7
Yuanyuan Zhou <sweetlucysweetme@gmail.com>
2019-11-07 14:53:55 -0500

    Initial Commit

---
+++ Aphasia App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Aphasia App
+//
+//  Created by Yuanyuan Zhou on 2019-11-07.
+//  Copyright © 2019 Yuanyuan Zhou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
