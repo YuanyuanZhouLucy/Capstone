4b1bd697ecfb8bd586b26254a2ead17bab8f5286
kbanc <katherine.bancoft@gmail.com>
2020-01-05 20:05:58 +0800

    added email functionality to progress report

---
+++ Aphasia App/CurrentProgressViewController.swift
@@ -0,0 +1,47 @@
+//
+//  CurrentProgressViewController.swift
+//  Aphasia App
+//
+//  Created by Katherine Bancroft on 2020-01-05.
+//  Copyright © 2020 Yuanyuan Zhou. All rights reserved.
+//
+
+import UIKit
+import MessageUI
+
+class CurrentProgressViewController: UIViewController, MFMailComposeViewControllerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func sendEmail(_ sender: UIButton) {
+        if !MFMailComposeViewController.canSendMail() {
+            print("Mail services are not available")
+            return
+        }
+        let composeVC = MFMailComposeViewController()
+        composeVC.mailComposeDelegate = self
+        self.present(composeVC, animated: true, completion: nil)
+    }
+    
+    func mailComposeController(controller: MFMailComposeViewController,
+                               didFinishWithResult result: MFMailComposeResult, error: NSError?) {
+        // Check the result or perform other tasks.
+        
+        // Dismiss the mail compose view controller.
+        controller.dismiss(animated: true, completion: nil)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
