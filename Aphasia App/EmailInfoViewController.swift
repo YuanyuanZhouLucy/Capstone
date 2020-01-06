cbafb12e6043f91ea3b034476fd4293012da1403
kbanc <katherine.bancoft@gmail.com>
2020-01-06 10:55:36 +0800

    made new view to collect email info

--- Aphasia App/EmailInfoViewController.swift
+++ Aphasia App/EmailInfoViewController.swift
@@ -1,16 +1,20 @@
 //
-//  CurrentProgressViewController.swift
+//  EmailInfoViewController.swift
 //  Aphasia App
 //
-//  Created by Katherine Bancroft on 2020-01-05.
+//  Created by Katherine Bancroft on 2020-01-06.
 //  Copyright © 2020 Yuanyuan Zhou. All rights reserved.
 //
 
 import UIKit
 import MessageUI
 
-class CurrentProgressViewController: UIViewController, MFMailComposeViewControllerDelegate {
+class EmailInfoViewController: UIViewController, MFMailComposeViewControllerDelegate {
 
+    @IBOutlet weak var clientNameTextField: UITextField!
+    @IBOutlet weak var slpNameTextField: UITextField!
+    @IBOutlet weak var slpEmailTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -24,6 +28,11 @@ class CurrentProgressViewController: UIViewController, MFMailComposeViewControll
         }
         let composeVC = MFMailComposeViewController()
         composeVC.mailComposeDelegate = self
+        
+        composeVC.setToRecipients(["address@example.com"])
+        composeVC.setSubject("Is this working?")
+        composeVC.setMessageBody("Sample body text", isHTML: false)
+        
         self.present(composeVC, animated: true, completion: nil)
     }
     
