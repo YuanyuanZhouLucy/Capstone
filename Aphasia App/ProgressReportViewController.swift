ae5056ba7382fb9bd563249de3bb81609ec0af42
kbanc <katherine.bancoft@gmail.com>
2020-01-12 19:03:33 -0500

    History table now works

--- Aphasia App/ProgressReportViewController.swift
+++ Aphasia App/ProgressReportViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ProgressReportViewController: UIViewController {
     @IBOutlet weak var exerciseALabel: UILabel!
     @IBOutlet weak var exerciseBLabel: UILabel!
+    @IBOutlet weak var historyBButton: UIButton!
     
     private var lastExerciseASession: SessionProgress?
     private var lastExerciseBSession: SessionProgress?
@@ -25,6 +26,15 @@ class ProgressReportViewController: UIViewController {
         self.modifyText(session: lastExerciseBSession, label: exerciseBLabel)
     }
     
+    func prepare(segue: UIStoryboardSegue, sender: Any?) {
+        let destinationVC = segue.destination as! ProjectHistoryViewController
+        if segue.identifier == "ExerciseAHistorySegue"{
+            destinationVC.whichExercise = "A"
+        }
+        else if segue.identifier == "ExerciseBHistorySegue"{
+            destinationVC.whichExercise = "B"
+        }
+    }
     
     func resultFormatter(session: SessionProgress) -> String {
         return "\(session.exercisesCorrect)/\(session.exercisesAttempted)"
@@ -37,5 +47,4 @@ class ProgressReportViewController: UIViewController {
         }
         label.text = "0/0"
     }
-
 }
