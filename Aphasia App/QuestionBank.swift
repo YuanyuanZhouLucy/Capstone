998175208eb3da07e862aee66043acdc742b7a75
Yuanyuan Zhou Lucy <sweetlucysweetme@gmail.com>
2020-03-01 19:10:21 -0500

    changed question numbers to maximum of 10 for each exercise

--- Aphasia App/QuestionBank.swift
+++ Aphasia App/QuestionBank.swift
@@ -25,27 +25,31 @@ class QuestionBank {
         print(locationTypeGV)
         print("the location i got is:")
         print(locationType)
-         if(locationTypeGV == "food" || locationTypeGV == "restaurant" || locationTypeGV == "cafe"){
-                    category = "Cafe"
-              }
-              else if(locationTypeGV == "grocery_or_supermarket"){
-                  category = "GroceryStore"
-              }
-              else if(locationTypeGV == "hospital"){
-                  category = "Hospital"
-              }
-              else if(locationTypeGV == "park"){
-                  category = "Park"
-              }
-              print("the category is:")
-              print(category)
-              ref.child(category).observeSingleEvent(of: .value, with:{(snapshot) in
+        if(locationTypeGV == "food" || locationTypeGV == "restaurant" || locationTypeGV == "cafe"){
+            category = "Cafe"
+        }
+        else if(locationTypeGV == "grocery_or_supermarket"){
+            category = "GroceryStore"
+        }
+        else if(locationTypeGV == "hospital"){
+            category = "Hospital"
+        }
+        else if(locationTypeGV == "park"){
+            category = "Park"
+        }
+        print("the category is:")
+        print(category)
+        ref.child(category).observeSingleEvent(of: .value, with:{(snapshot) in
             let value  = snapshot.value as? NSDictionary
             let value2  = snapshot.value as? NSDictionary
             let value3  = snapshot.value as? NSDictionary
             let value4  = snapshot.value as? NSDictionary
             let value5  = snapshot.value as? NSDictionary
-            
+            let value6  = snapshot.value as? NSDictionary
+            let value7  = snapshot.value as? NSDictionary
+            let value8  = snapshot.value as? NSDictionary
+            let value9  = snapshot.value as? NSDictionary
+            let value10  = snapshot.value as? NSDictionary
             func makeList(_ n: Int) -> [Int] {
                 return (0..<n).map { _ in .random(in: 1...5) }
             }
@@ -73,7 +77,7 @@ class QuestionBank {
             let    randomNonRelatedWordPicker2 = Int.random(in: 0..<sizeList2)
             
             
-          
+            
             let Exercise = value?["Exercise" + String(randomNumbers[0])] as? NSDictionary
             let imageURL = Exercise?["ImageURL"] as? String ?? ""
             let answer = Exercise?["Answer"] as? String ?? ""
@@ -91,7 +95,7 @@ class QuestionBank {
             let wrongOpt1 = Exercise?["WrongOpt"] as? Int ?? 1
             
             print("inside question bank")
-                print(wrongOpt1)
+            print(wrongOpt1)
             let Exercise2 = value2?["Exercise" + String(randomNumbers[1])] as? NSDictionary
             let imageURL2 = Exercise2?["ImageURL"] as? String ?? ""
             let answer2 = Exercise2?["Answer"] as? String ?? ""
@@ -140,7 +144,7 @@ class QuestionBank {
             let r44 = Exercise4?["Opt4"] as? String ?? ""
             let wrongOpt4 = Exercise4?["WrongOpt"] as? Int ?? 1
             
-                
+            
             let Exercise5 = value5?["Exercise" + String(randomNumbers[4])] as? NSDictionary
             let imageURL5 = Exercise5?["ImageURL"] as? String ?? ""
             let answer5 = Exercise5?["Answer"] as? String ?? ""
@@ -157,16 +161,110 @@ class QuestionBank {
             let r54 = Exercise5?["Opt4"] as? String ?? ""
             let wrongOpt5 = Exercise5?["WrongOpt"] as? Int ?? 1
             
+            let Exercise6 = value6?["Exercise6" ] as? NSDictionary
+            let imageURL6 = Exercise6?["ImageURL"] as? String ?? ""
+            let answer6 = Exercise6?["Answer"] as? String ?? ""
+            let wrong61=Exercise6?["Wrong1"] as? String ?? ""
+            let wrong62=Exercise6?["Wrong2"] as? String ?? ""
+            let wrong63=Exercise6?["Wrong3"] as? String ?? ""
+            let cue61 = Exercise6?["Cue1"] as? String ?? ""
+            let cue62 = Exercise6?["Cue2"] as? String ?? ""
+            let cue63 = Exercise6?["Cue3"] as? String ?? ""
+            let cue64 = Exercise6?["Cue4"] as? String ?? ""
+            let r61 = Exercise6?["Opt1"] as? String ?? ""
+            let r62 = Exercise6?["Opt2"] as? String ?? ""
+            let r63 = Exercise6?["Opt3"] as? String ?? ""
+            let r64 = Exercise6?["Opt4"] as? String ?? ""
+            let wrongOpt6 = Exercise6?["WrongOpt"] as? Int ?? 1
+            
+            
+            let Exercise7 = value7?["Exercise7"] as? NSDictionary
+            let imageURL7 = Exercise7?["ImageURL"] as? String ?? ""
+            let answer7 = Exercise7?["Answer"] as? String ?? ""
+            let wrong71=Exercise7?["Wrong1"] as? String ?? ""
+            let wrong72=Exercise7?["Wrong2"] as? String ?? ""
+            let wrong73=Exercise7?["Wrong3"] as? String ?? ""
+            let cue71 = Exercise7?["Cue1"] as? String ?? ""
+            let cue72 = Exercise7?["Cue2"] as? String ?? ""
+            let cue73 = Exercise7?["Cue3"] as? String ?? ""
+            let cue74 = Exercise7?["Cue4"] as? String ?? ""
+            let r71 = Exercise7?["Opt1"] as? String ?? ""
+            let r72 = Exercise7?["Opt2"] as? String ?? ""
+            let r73 = Exercise7?["Opt3"] as? String ?? ""
+            let r74 = Exercise7?["Opt4"] as? String ?? ""
+            let wrongOpt7 = Exercise7?["WrongOpt"] as? Int ?? 1
+            
+            
+            let Exercise8 = value8?["Exercise8"] as? NSDictionary
+            let imageURL8 = Exercise8?["ImageURL"] as? String ?? ""
+            let answer8 = Exercise8?["Answer"] as? String ?? ""
+            let wrong81=Exercise8?["Wrong1"] as? String ?? ""
+            let wrong82=Exercise8?["Wrong2"] as? String ?? ""
+            let wrong83=Exercise8?["Wrong3"] as? String ?? ""
+            let cue81 = Exercise8?["Cue1"] as? String ?? ""
+            let cue82 = Exercise8?["Cue2"] as? String ?? ""
+            let cue83 = Exercise8?["Cue3"] as? String ?? ""
+            let cue84 = Exercise8?["Cue4"] as? String ?? ""
+            let r81 = Exercise8?["Opt1"] as? String ?? ""
+            let r82 = Exercise8?["Opt2"] as? String ?? ""
+            let r83 = Exercise8?["Opt3"] as? String ?? ""
+            let r84 = Exercise8?["Opt4"] as? String ?? ""
+            let wrongOpt8 = Exercise8?["WrongOpt"] as? Int ?? 1
+            
+            
+            let Exercise9 = value9?["Exercise9"] as? NSDictionary
+            let imageURL9 = Exercise9?["ImageURL"] as? String ?? ""
+            let answer9 = Exercise9?["Answer"] as? String ?? ""
+            let wrong91=Exercise9?["Wrong1"] as? String ?? ""
+            let wrong92=Exercise9?["Wrong2"] as? String ?? ""
+            let wrong93=Exercise9?["Wrong3"] as? String ?? ""
+            let cue91 = Exercise9?["Cue1"] as? String ?? ""
+            let cue92 = Exercise9?["Cue2"] as? String ?? ""
+            let cue93 = Exercise9?["Cue3"] as? String ?? ""
+            let cue94 = Exercise9?["Cue4"] as? String ?? ""
+            let r91 = Exercise9?["Opt1"] as? String ?? ""
+            let r92 = Exercise9?["Opt2"] as? String ?? ""
+            let r93 = Exercise9?["Opt3"] as? String ?? ""
+            let r94 = Exercise9?["Opt4"] as? String ?? ""
+            let wrongOpt9 = Exercise9?["WrongOpt"] as? Int ?? 1
+            
+            
+            
+            let Exercise10 = value10?["Exercise10" ] as? NSDictionary
+            let imageURL10 = Exercise10?["ImageURL"] as? String ?? ""
+            let answer10 = Exercise10?["Answer"] as? String ?? ""
+            let wrong101=Exercise10?["Wrong1"] as? String ?? ""
+            let wrong102=Exercise10?["Wrong2"] as? String ?? ""
+            let wrong103=Exercise10?["Wrong3"] as? String ?? ""
+            let cue101 = Exercise10?["Cue1"] as? String ?? ""
+            let cue102 = Exercise10?["Cue2"] as? String ?? ""
+            let cue103 = Exercise10?["Cue3"] as? String ?? ""
+            let cue104 = Exercise10?["Cue4"] as? String ?? ""
+            let r101 = Exercise10?["Opt1"] as? String ?? ""
+            let r102 = Exercise10?["Opt2"] as? String ?? ""
+            let r103 = Exercise10?["Opt3"] as? String ?? ""
+            let r104 = Exercise10?["Opt4"] as? String ?? ""
+            let wrongOpt10 = Exercise10?["WrongOpt"] as? Int ?? 1
+            
+            
+            
+            
             self.list.append(Question(image: imageURL, optA: answer, optB: wrong11,optC:wrong12,optD:wrong13, answer: 1,q1:cue11,q2:cue12,q3:cue13,q4:cue14,r1:r11,r2:r12,r3:r13,r4:r14,wrongOption: wrongOpt1))
             self.list.append(Question(image: imageURL2, optA: wrong21, optB: answer2,optC:wrong22,optD:wrong23, answer: 2,q1:cue21,q2:cue22,q3:cue23,q4:cue24,r1:r21,r2:r22,r3:r23,r4:r24,wrongOption: wrongOpt2))
             self.list.append(Question(image: imageURL3, optA: answer3, optB: wrong31,optC:wrong32,optD:wrong33, answer: 1,q1:cue31,q2:cue32,q3:cue33,q4:cue34,r1:r31,r2:r32,r3:r33,r4:r34,wrongOption: wrongOpt3))
             self.list.append(Question(image: imageURL4, optA: wrong41, optB: answer4,optC:wrong42,optD:wrong43, answer: 2,q1:cue41,q2:cue42,q3:cue43,q4:cue44,r1:r41,r2:r42,r3:r43,r4:r44, wrongOption: wrongOpt4))
             self.list.append(Question(image: imageURL5, optA: answer5, optB:wrong51,optC:wrong52,optD:wrong53, answer: 1,q1:cue51,q2:cue52,q3:cue53,q4:cue54,r1:r51,r2:r52,r3:r53,r4:r54,wrongOption: wrongOpt5))
-                self.size = 5
+            self.size = 5
             
+            self.list.append(Question(image: imageURL6, optA: answer6, optB: wrong61,optC:wrong62,optD:wrong63, answer: 1,q1:cue61,q2:cue62,q3:cue63,q4:cue64,r1:r61,r2:r62,r3:r63,r4:r64,wrongOption: wrongOpt6))
+            self.list.append(Question(image: imageURL7, optA: wrong71, optB: answer7,optC:wrong72,optD:wrong73, answer: 2,q1:cue71,q2:cue72,q3:cue73,q4:cue74,r1:r71,r2:r72,r3:r73,r4:r74,wrongOption: wrongOpt7))
+            self.list.append(Question(image: imageURL8, optA: answer8, optB: wrong81,optC:wrong82,optD:wrong83, answer: 1,q1:cue81,q2:cue82,q3:cue83,q4:cue84,r1:r81,r2:r82,r3:r83,r4:r84,wrongOption: wrongOpt8))
+            self.list.append(Question(image: imageURL9, optA: wrong91, optB: answer9,optC:wrong92,optD:wrong93, answer: 2,q1:cue91,q2:cue92,q3:cue93,q4:cue94,r1:r91,r2:r92,r3:r93,r4:r94, wrongOption: wrongOpt9))
+            self.list.append(Question(image: imageURL10, optA: answer10, optB:wrong101,optC:wrong102,optD:wrong103, answer: 1,q1:cue101,q2:cue102,q3:cue103,q4:cue104,r1:r101,r2:r102,r3:r103,r4:r104,wrongOption: wrongOpt10))
+            self.size = 10
         })
         
-      
+        
         ref.child("userDefinedEx").child("uid\(uid)").observeSingleEvent(of: .value, with: {(snapshot1)in
             if snapshot1.hasChild(self.category){
                 self.ref.child("userDefinedEx").child("uid\(self.uid)").child(self.category).observeSingleEvent(of: .value, with:{(snapshot) in
@@ -187,9 +285,9 @@ class QuestionBank {
                         let r13 = (snap.value as? NSDictionary)?["Opt3"] as? String ?? ""
                         let r14 = (snap.value as? NSDictionary)?["Opt4"] as? String ?? ""
                         let wrongOpt1 = (snap.value as? NSDictionary)?["WrongOpt"] as? Int ?? 1
-                          self.list.append(Question(image: imageURL, optA: answer, optB: wrong11,optC:wrong12,optD:wrong13, answer: 1,q1:cue11,q2:cue12,q3:cue13,q4:cue14,r1:r11,r2:r12,r3:r13,r4:r14,wrongOption: wrongOpt1))
+                        self.list.append(Question(image: imageURL, optA: answer, optB: wrong11,optC:wrong12,optD:wrong13, answer: 1,q1:cue11,q2:cue12,q3:cue13,q4:cue14,r1:r11,r2:r12,r3:r13,r4:r14,wrongOption: wrongOpt1))
                         
-                        self.size = 6
+                        self.size = 11
                     }
                 })
             }
