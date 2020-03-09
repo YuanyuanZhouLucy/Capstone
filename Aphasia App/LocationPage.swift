e00c2e65e3523c0a4e2f03c241856d4356f38226
Yuanyuan Zhou Lucy <sweetlucysweetme@gmail.com>
2020-03-08 23:33:06 -0400

    resolved conflicts and brought back my changes

--- Aphasia App/LocationPage.swift
+++ Aphasia App/LocationPage.swift
@@ -77,9 +77,9 @@ class LocationPage: UIViewController {
                     print("address")
                     print(self.address)
                     locationTypeGV = self.locationType
-                    self.goToExerciseButton.isHidden = false
                     
-                   // locationTypeGV = "me"
+                    
+                  //  locationTypeGV = "me"
                     if (locationTypeGV !=  "food" && locationTypeGV != "restaurant" && locationTypeGV != "cafe" && locationTypeGV != "hospital"  && locationTypeGV != "grocery_or_supermarket" && locationTypeGV != "park"){
                         
                         self.defaultRequestLabel.isHidden = false
@@ -88,6 +88,7 @@ class LocationPage: UIViewController {
                         self.groceryButton.isHidden = false
                         self.hospitalButton.isHidden = false
                     }
+                    else{ self.goToExerciseButton.isHidden = false}
                 }
             }
         })
